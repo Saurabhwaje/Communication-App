977a7bc430554a6d9a8c70b4c9fa68b8e78c427f
S A U R A B H  W A J E <saurabhwaje4770@gmail.com>
2024-01-12 16:54:28 +0530

    Update app-routing.module.ts

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -27,3 +27,27 @@ const routes: Routes = [
 export class AppRoutingModule {
 }
 
+// Eager Loading End
+
+// Lazy Loading Start
+
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+
+// const routes: Routes = [
+//   { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+//   { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+//   { path: 'register', loadChildren: () => import('./register/register.module').then(m => m.RegisterModule) },
+//   { path: 'sendmessage', loadChildren: () => import('./sendmessage/sendmessage.module').then(m => m.SendMessageModule) },
+//   { path: 'chat', loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule) },
+//   { path: 'chat/:id', loadChildren: () => import('./chat/chat.module').then(m => m.ChatModule) },
+//   { path: 'chatwindowstatic', loadChildren: () => import('./chatwindowstatic/chatwindowstatic.module').then(m => m.ChatWindowStaticModule) },
+//   { path: 'chatwindowstatic/:id', loadChildren: () => import('./chatwindowstatic/chatwindowstatic.module').then(m => m.ChatWindowStaticModule) },
+//   { path: 'twiliochat', loadChildren: () => import('./twilio-chat/twilio-chat.module').then(m => m.TwilioChatModule) }
+// ];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
